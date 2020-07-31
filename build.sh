99b8805ba0d21a6fc6d106f162cab0ad00c03cd5
P Chen <figroc@gmail.com>
2020-07-31 14:13:54 +0800

    upgrade to 2.3

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 cd $(dirname ${BASH_SOURCE[0]})
 
 builder="figroc/tfsclient:build"
-if [[ -z "$(docker images -q ${builder})"]]; then
+if [[ -z "$(docker images -q ${builder})" ]]; then
   docker build -t ${builder} .
 fi
 docker run --rm -it -v $(pwd):/work \
