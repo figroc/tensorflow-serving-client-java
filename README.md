9bab8896a0c79a798484003c96ec802b494b18be
figroc <figroc@gmail.com>
2021-07-03 21:47:36 +0800

    upgrade to 2.4 (#17)
    * upgrade to 2.4
    
    * update grpc&pb
    
    * fix rust build
    
    * upgrade nodejs
    
    * donot update homebrew
    
    * use default osx
    
    * use docker build
    
    * try nodejs 16
    
    * install python-dev
    
    * update python3.8
    
    * fix pip install
    
    * fix docker build
    
    * pull builder in ci
    
    * try github ci
    
    * fix workflow syntax
    
    * fixing docker run
    
    * try user mapping
    
    * fix run syntax
    
    * try mapping in build.sh
    
    * try chmod approach
    
    * more specific chmod
    
    * fix tty allocate
    
    * fix artifact name
    
    * fix workspace

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tensorflow-serving-client
 
-[![Build Status](https://travis-ci.com/figroc/tensorflow-serving-client.svg?branch=master)](https://travis-ci.com/figroc/tensorflow-serving-client)
+![Build Status](https://github.com/figroc/tensorflow-serving-client/actions/workflows/main.yml/badge.svg)
 
 A prebuilt tensorflow serving client from the tensorflow serving proto files.
 
@@ -25,7 +25,7 @@ Grpc tools are needed for building variant packages.
 
 See `.travis.yml` for details.
 
-*NOTE: grpc@1.32 and protobuf@3.13 are required*
+*NOTE: grpc@1.35 and protobuf@3.14 are required*
 
 Target | Command         | Outputs     | Artifacts
 -------|-----------------|-------------|----------------------------------------
--- build.sh
+++ build.sh
@@ -4,18 +4,27 @@ set -e
 cd $(dirname ${BASH_SOURCE[0]})
 
 builder="figroc/tfsclient:build"
+
 if [[ -z "$(docker images -q ${builder})" ]]; then
-  docker build -t ${builder} .
+  docker build -t ${builder} --build-arg HTTP_PROXY .
 fi
 
-if [[ "$(uname)" != "Darwin" && "$(id -u)" != "1000" ]]; then
+if [[ "$(id -u)" != "1000" ]]; then
   (
-    echo "WARNING: Current user $(id -F)($(id -u):$(id -g))" \
+    echo "WARNING: Current user $(whoami)($(id -u):$(id -g))" \
          "does not match the user gradle(1000:1000) in the container."
     echo "The building might fail for unable to access dirs:" \
          ".gralde, build and obj."
   ) 1>&2
+  for d in .gradle build obj; do
+    mkdir -p ${d}
+    chmod -R go+u ${d}
+  done
+  chmod go+u .
 fi
 
-docker run --rm -it -v $(pwd):/work \
-  ${builder} gradle --no-daemon $@
+workspace=/work/tensorflow-serving-client
+
+docker run --rm -w=${workspace} \
+  -e HTTP_PROXY -e GOPROXY -e ALL_PROXY=${HTTP_PROXY} \
+  -v $(pwd):${workspace} ${builder} gradle --no-daemon "$@"
