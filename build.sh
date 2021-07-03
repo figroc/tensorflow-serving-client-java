9ec2619c6b5da084a14ec4e4a236f99a836da92f
P Chen <figroc@gmail.com>
2021-07-03 20:32:00 +0800

    more specific chmod

--- build.sh
+++ build.sh
@@ -9,14 +9,17 @@ if [[ -z "$(docker images -q ${builder})" ]]; then
   docker build -t ${builder} --build-arg HTTP_PROXY .
 fi
 
-if [[ "$(uname)" != "Darwin" && "$(id -u)" != "1000" ]]; then
+if [[ "$(id -u)" != "1000" ]]; then
   (
-    echo "WARNING: Current user $(id -F)($(id -u):$(id -g))" \
+    echo "WARNING: Current user $(id -n)($(id -u):$(id -g))" \
          "does not match the user gradle(1000:1000) in the container."
     echo "The building might fail for unable to access dirs:" \
          ".gralde, build and obj."
   ) 1>&2
-  chmod go+u .
+  for d in .gradle build obj; do
+    mkdir -p ${d}
+    chmod -R go+u ${d}
+  done
 fi
 
 docker run --rm -it \
