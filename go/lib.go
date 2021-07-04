6632d0d1eea77279efb298f69ce0dff20b7f042a
P Chen <figroc@gmail.com>
2021-07-04 16:11:33 +0800

    release sources

---
+++ go/lib.go
@@ -0,0 +1,7 @@
+package tfsclient
+
+import (
+  _ "github.com/golang/protobuf/ptypes"
+  _ "google.golang.org/grpc"
+  _ "github.com/figroc/tensorflow-serving-client/v2/go/tensorflow_serving/apis"
+)
--- lib.go
+++
@@ -1,7 +0,0 @@
-package tfsclient
-
-import (
-  _ "github.com/golang/protobuf/ptypes"
-  _ "google.golang.org/grpc"
-  _ "tensorflow_serving/apis"
-)
